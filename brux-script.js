49f7478a5afde46f211ff27b334d1c5295085c11
BrujaVudu <brujaaa@gmail.com>
2017-11-12 13:14:16 -0500

    Abregar BRUX al menú

--- brux-script.js
+++ brux-script.js
@@ -8,13 +8,16 @@ function cabecera() {
 function fueraTitulo() {
   var titulo = document.getElementById("titulo");
   var menu = document.getElementById("menu");
+  var brux = document.getElementById("brux");
   
   if (estanAfuera) {
     titulo.className = "titulo";
     menu.className = "menu inselecto";
+    brux.className = "brux";
   } else {
     titulo.className += " fueraTitulo";
     menu.className += " fueraMenu";
+    brux.className += " verBrux";
   }
   
   estanAfuera = !estanAfuera;
