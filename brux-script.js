b958a95119056b9eb9a267676313f732a5399fd3
BrujaVudu <brujaaa@gmail.com>
2017-11-18 22:29:27 -0500

    Poner título de la presentación y otros ajustes

--- brux-script.js
+++ brux-script.js
@@ -5,22 +5,25 @@ function cabecera() {
   cabeza.innerHTML = '<div class="prueba">Hola, putos</div>';
 }
 
-function fueraTitulo() {
-  var titulo = document.getElementById("titulo");
+function fueraLogo() {
+  var logo = document.getElementById("logo");
   var menu = document.getElementById("menu");
   var brux = document.getElementById("brux");
   var fondo = document.getElementById("fondo");
+  var presetacion = document.getElementById("presentacion");
   
   if (estanAfuera) {
-    titulo.className = "titulo";
+    logo.className = "logo";
     menu.className = "menu inselecto";
     brux.className = "brux";
     fondo.className = "fondo";
+    presentacion.className = "presentacion";
   } else {
-    titulo.className += " fueraTitulo";
+    logo.className += " fueraLogo";
     menu.className += " fueraMenu";
     brux.className += " verBrux";
     fondo.className += " fondoPeque";
+    presentacion.className += " entraPresentacion";
   }
   
   estanAfuera = !estanAfuera;
