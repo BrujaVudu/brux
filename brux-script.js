b89f1d6b3fb1dc89810f79a4805f9d50bc655c04
BrujaVudu <brujaaa@gmail.com>
2017-11-18 20:58:20 -0500

    Mejoras varias

--- brux-script.js
+++ brux-script.js
@@ -9,15 +9,18 @@ function fueraTitulo() {
   var titulo = document.getElementById("titulo");
   var menu = document.getElementById("menu");
   var brux = document.getElementById("brux");
+  var fondo = document.getElementById("fondo");
   
   if (estanAfuera) {
     titulo.className = "titulo";
     menu.className = "menu inselecto";
     brux.className = "brux";
+    fondo.className = "fondo";
   } else {
     titulo.className += " fueraTitulo";
     menu.className += " fueraMenu";
     brux.className += " verBrux";
+    fondo.className += " fondoPeque";
   }
   
   estanAfuera = !estanAfuera;
