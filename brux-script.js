71b7f48d85418ab51ed6029f1cb26ea0c75c7a52
BrujaVudu <brujaaa@gmail.com>
2017-12-12 08:41:07 -0500

    All y otros cambios a Brux

--- brux-script.js
+++ brux-script.js
@@ -18,13 +18,28 @@ function fueraLogo() {
     brux.className = "brux";
     fondo.className = "fondo";
     presentacion.className = "presentacion";
+    setTimeout(function(){ presentacion.style.display = "none"; }, 100);
   } else {
     logo.className += " fueraLogo";
     menu.className += " fueraMenu";
     brux.className += " verBrux";
     fondo.className += " fondoPeque";
-    presentacion.className += " entraPresentacion";
+    presentacion.style.display = "block"
+    setTimeout(function(){ presentacion.className += " entraPresentacion"; }, 500);
   }
   
   estanAfuera = !estanAfuera;
 }
+
+
+$(function () {
+  $.get(
+    "https://api.airtable.com/v0/appAhDvzAtHAWleAz/Table%201?api_key=keyD8IYkmIDejDWut",
+    function (data) {
+      $.each(data.records,
+        function (i, e) {
+          i = 1+i;
+          $("#resumen").append("<h1>"+i+" "+  e.fields.titulo + "</h1><p>"+e.fields.texto.replace(/\n/g,"<br>")+"</p>");
+        });
+      });
+    });
